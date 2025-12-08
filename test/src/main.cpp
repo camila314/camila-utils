f2b7d1b650bb41afc12f6a66c7a9b6515bd518ea
camila314 <47485054+camila314@users.noreply.github.com>
2025-12-07 18:02:00 -0600

    huge

--- test/src/main.cpp
+++ test/src/main.cpp
@@ -1,6 +1,9 @@
+#include "Geode/utils/Task.hpp"
 #include <Geode/Geode.hpp>
 
 #include <camila/utils.hpp>
+#include <concepts>
+#include <cpp-reactive.hpp>
 #include <UIBuilder.hpp>
 
 #include <Geode/utils/coro.hpp>
@@ -8,76 +11,12 @@
 using namespace geode::prelude;
 using namespace camila;
 
-template <typename Data>
-class ScrollableListView : public ScrollLayer {
-    cppreactive::Signal<std::vector<Data>> m_data;
-    std::function<void(camila::CCMenuColor*, Data const&, int)> m_cellSetup;
-    Observatory m_observatory;
-    Signal<CCSize> m_contentSize;
- public:
-    void build(float width) {
-        m_contentLayer->removeAllChildren();
-
-        int i = 0;
-        for (auto const& d : *m_data) {
-            auto cell = camila::CCMenuColor::create();
-            cell->setContentWidth(width);
-            m_cellSetup(cell, d, i++);
-            m_contentLayer->addChild(cell);
-        }
-    }
-
-    void update(float) override {
-        float before = m_contentLayer->getContentHeight();
-        m_contentLayer->updateLayout();
-        float after = m_contentLayer->getContentHeight();
-
-        if (before != after) {
-            m_contentLayer->setPositionY(m_contentLayer->getPositionY() + before - after);
-            scrollWheel(0, 0);
-        }
-
-        if (*m_contentSize != getContentSize()) {
-            *m_contentSize = getContentSize();
-        }
-    }
-
-    ScrollableListView(decltype(m_cellSetup)&& cellSetup, std::vector<Data>&& vec) : ScrollLayer(CCRectZero, true, true) {
-        *m_data = std::move(vec);
-        m_cellSetup = std::move(cellSetup);
-
-        ignoreAnchorPointForPosition(false);
-        scheduleUpdate();
-
-        m_observatory.reactToChanges([&] {
-            m_contentLayer->setLayout(ColumnLayout::create()
-                ->setGap(0)
-                ->setAutoGrowAxis(m_contentSize->get().height)
-                ->setAxisAlignment(AxisAlignment::End)
-                ->setAxisReverse(true));
-
-            this->build(m_contentSize->get().width);
-        });
-    }
-
-    auto& data() {
-        return *m_data;
-    }
-
-    static ScrollableListView* create(decltype(m_cellSetup)&& cellSetup, std::vector<Data>&& vec = {}) {
-        auto ret = new ScrollableListView(std::move(cellSetup), std::move(vec));
-        ret->autorelease();
-        return ret;
-    }
-};
-
 #include <Geode/modify/MenuLayer.hpp>
 class $modify(MenuLayer) {
-
     bool init() {
         if (!MenuLayer::init()) return false;
 
-        auto lv = ScrollableListView<std::string>::create([](camila::CCMenuColor* cell, std::string const& text, int idx) {
+        auto lv = camila::ScrollableListView<std::string>::create([](camila::CCMenuColor* cell, std::string const& text, int idx) {
             Build(cell)
                 .color(idx % 2 == 0 ? ccc3(150, 80, 40) : ccc3(40, 80, 150))
                 .height(30)
@@ -92,6 +31,15 @@ class $modify(MenuLayer) {
 
         lv->data()[3] = "modified!";
 
+        $async() {        
+            auto confirm = Confirmation::create("Do you <cr>want</c> to?", {.noBtn = "No way!"});
+            
+            confirm->show();
+            bool result = co_await confirm->task();
+
+            log::info("Confirmation result: {}", result ? "Yes" : "No");
+            co_return;
+        };
         return true;
     }
 
